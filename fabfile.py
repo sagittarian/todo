68a6bbfc8f345665e36e27c946de9b94880cc06d
Adam Mesha <adam@mesha.org>
2013-05-01 00:34:57 +0300

    Some basic fabric commands.

---
+++ fabfile.py
@@ -0,0 +1,27 @@
+from fabric.api import *
+import os.path
+
+env.user = 'mesha'
+env.hosts = ['mesha.webfactional.com']
+
+BASEDIR = '/home/amesha/projects/todo'
+
+def coffee():
+    with lcd(BASEDIR):
+        with lcd('todo/static/js'):
+            local('coffee -c todo.coffee')
+        local('echo yes | ./manage.py collectstatic')
+
+def watch_cs():
+    with lcd(os.path.join(BASEDIR, 'todo/static/js')):
+        local('coffee -cw todo.coffee')
+
+def buildjs():
+    coffee()
+    with lcd(BASEDIR):
+        files = ['jquery-el/jquery-el.js',
+                 'jquery_jeditable/js/jquery.jeditable.js',
+                 'todo/static/js/todo.js']
+        local('cat {} > todo/static/js/todo.all.js'.format(' '.join(files)))
+        with lcd('todo/static/js'):
+            local('yui-compressor todo.all.js -o todo.min.js')
