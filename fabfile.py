4ee2751113ed00e83deba305edf54d43c875615a
Adam Mesha <adam@mesha.org>
2013-05-01 09:54:02 +0300

    Deploy command.

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,6 @@
 from fabric.api import *
 import os.path
+from fabric.context_managers import prefix
 
 env.user = 'mesha'
 env.hosts = ['mesha.webfactional.com']
@@ -26,3 +27,15 @@ def buildjs():
         local('cat {} > todo/static/js/todo.all.js'.format(' '.join(files)))
         with lcd('todo/static/js'):
             local('yui-compressor todo.all.js -o todo.min.js')
+
+def restart_remote():
+    with cd('/home/mesha/webapps/todo_backend/apache2/bin'):
+        run('./restart')
+
+def deploy():
+    with cd('/home/mesha/webapps/todo_backend/todo'):
+        with prefix('source venv/bin/activate'):
+            run('git pull origin master')
+            run('echo "yes" | ./manage.py collectstatic')
+            run('./manage.py migrate todolist')
+    restart_remote()
