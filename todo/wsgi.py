91e198731813c5de7c56ec38b431cb54da7241b0
Adam Mesha <adam@mesha.org>
2013-05-01 09:00:37 +0300

    Find the virtualenvironment in the wsgi script.

--- todo/wsgi.py
+++ todo/wsgi.py
@@ -14,12 +14,21 @@ framework.
 
 """
 import os
+import sys
+import site
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "todo.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
+
+this_dir = os.path.dirname(os.path.realpath(__file__))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+path = os.path.abspath(os.path.join(this_dir, '..'))
+site.addsitedir(os.path.join(this_dir, '../venv/lib/python2.7/site-packages'))
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
